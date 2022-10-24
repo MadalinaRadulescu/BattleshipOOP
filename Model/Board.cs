441ce5946d9be25abcf35aba7b7440b732784c13
MadalinaRadulescu <radulescumadalina90@yahoo.com>
2022-10-24 12:35:53 +0300

    class board almost done

--- Model/Board.cs
+++ Model/Board.cs
@@ -2,5 +2,21 @@ namespace Battleship.Model;
 
 public class Board
 {
-    
+    private Square[,] ocean { get; set; }
+    private int size = 15;
+
+    public Board()
+    {
+        ocean = new Square[size, size];
+        for (int row = 0; row < size; row++)
+        {
+            for (int col = 0; col < size; col++)
+            {
+                ocean[row, col] = new Square((row, col), Status.empty);
+            }
+        }
+    }
+
+    public bool isPlacementOK() => throw new NotImplementedException();
+
 }
